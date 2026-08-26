b0db0e6b01906ac79f65b91765379859bbfb8099
Jairaj Munirathnam <jairaj.kingofwinners@gmail.com>
2026-08-26 20:38:35 +0530

    Add Userscript wrapper and clean minified bookmarklet

---
+++ bb_deal_finder.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         BigBasket Deal Sniper (Auto-Update from GitHub)
+// @namespace    https://github.com/jairaj26/bigbasket-deals
+// @version      1.0
+// @description  Find flash deals on BigBasket across categories
+// @author       jairaj26
+// @match        *://*.bigbasket.com/*
+// @grant        none
+// @require      https://raw.githubusercontent.com/jairaj26/bigbasket-deals/main/bb_deal_finder.js
+// ==/UserScript==
